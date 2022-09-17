0bdeb3ae33c2da67b4ddb37df60fe2df9f731a4f
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2022-09-17 14:44:47 +0300

    bravo!

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const Alert = React.forwardRef(function Alert(props, ref) {
   return (
     <Stack spacing={0} sx={{ width: '90%' }} display={"flex"} direction={"row"} justifyContent={"space-between"} margin={5} borderRadius={"12px"}>
       
-     <Alert severity="warning" color={"info"} justify={"center"} >
+     <Alert severity="warning" color={"info"} justify={"center"} sx={{borderRadius: "12px"}}>
       <Typography>  
       <p>Info Message</p>
       <p>Some message</p>
@@ -22,20 +22,20 @@ const Alert = React.forwardRef(function Alert(props, ref) {
        <img src={x} alt="" />
         </Alert>
 
-        <Alert severity="warning" color={"success"}>
+        <Alert severity="warning" color={"success"} sx={{borderRadius: "12px"}}>
         <Typography><p>Succes Message</p><p>Some message</p></Typography>
         <img src={x} alt="" />
         </Alert>
 
         
-      <Alert severity="warning" color={"warning"}>
+      <Alert severity="warning" color={"warning"} sx={{borderRadius: "12px"}}>
 
         <Typography><p>Warning Message</p><p>Some message</p></Typography>
     
       <img src={x} alt="" />
       </Alert>
 
-      <Alert severity="warning" color={"error"}>
+      <Alert severity="warning" color={"error"} sx={{borderRadius: "12px"}}>
         <Typography><p>Error Message</p><p>Some message</p></Typography>
       <img src={x} alt="" />
       </Alert>
